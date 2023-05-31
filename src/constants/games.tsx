4bc26d91d55b07190fab43e8f82487c010f3438f
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2023-05-31 23:10:49 +0300

    images way

--- src/constants/games.tsx
+++ src/constants/games.tsx
@@ -1,6 +1,6 @@
 export const GAMES = [
     {
-        image: '/assets/forza_5.jpeg',
+        image: './assets/forza_5.jpeg',
         title: 'Forza Horizon 5',
         genres: ['Гонки', 'Симулятор', 'Відкритий світ'],
         price: 2343,
@@ -9,7 +9,7 @@ export const GAMES = [
         description: "Вас чекає нескінченний калейдоскоп пригод Horizon! Здійснюйте захоплюючі поїздки неймовірно красивим і самобутнім світом Мексики за кермом найбільших автомобілів в історії. Почніть свою пригоду Horizon вже сьогодні, додавши гру до свого списку бажань!",
     },
     {
-        image: '/assets/battlefield_2042.jpg',
+        image: './assets/battlefield_2042.jpg',
         title: 'Battlefield 2042',
         genres: ['Екшен', 'Шутер', 'Війна'],
         video: 'https://www.youtube.com/embed/ASzOzrB-a9E',
@@ -18,7 +18,7 @@ export const GAMES = [
         description: 'Battlefield™ 2042 – це шутер від першої особи, в якому серія повертається до легендарних масштабних битв. У майбутньому, де панує хаос, адаптуйтеся і процвітайте на полях бою, що постійно змінюються, завдяки своєму загону і арсеналу новітніх технологій.'
     },
     {
-        image: '/assets/life_is_strange_true_colors.jpeg',
+        image: './assets/life_is_strange_true_colors.jpeg',
         title: 'Life is Strange True Colors',
         genres: ['Глубокий сюжет', 'Протагоника'],
         video: 'https://www.youtube.com/embed/b6CkzwVAr0M',
@@ -27,7 +27,7 @@ export const GAMES = [
         description: 'Алекс Чень від усіх приховує своє «прокляття» — надприродну здатність зчитувати сильні емоції інших та впливати на них. Але коли її брат гине — нібито внаслідок нещасного випадку, Алекс використовує її, щоб дізнатися правду.'
     },
     {
-        image: '/assets/gta_v.jpeg',
+        image: './assets/gta_v.jpeg',
         title: 'Grand Theft Auto V',
         genres: ['Відкритий світ', 'Екшен'],
         video: 'https://www.youtube.com/embed/QkkoHAzjnUs',
@@ -36,7 +36,7 @@ export const GAMES = [
         description: 'Grand Theft Auto V для PC дозволяє гравцям дослідити знаменитий світ Лос-Сантоса та округу Блейн у роздільній здатності до 4k і вище з частотою 60 кадрів на секунду.'    
     },
     {
-        image: '/assets/rainbow_siege.jpeg',
+        image: './assets/rainbow_siege.jpeg',
         title: 'Tom Clancy\'s Rainbow Six® Siege',
         video: 'https://www.youtube.com/embed/6wlvYh0h63k',
         genres: ['Тактика', 'Шутер'],
@@ -45,7 +45,7 @@ export const GAMES = [
         description: 'Tom Clancy\'s Rainbow Six Облога – це продовження гучного шутера від першої особи, розробленої студією Ubisoft Montreal.'    
     },
     {
-        image: '/assets/assassins_creed_valhalla.png',
+        image: './assets/assassins_creed_valhalla.png',
         title: 'Assassin’s Creed Valhalla',
         genres: ['Паркур', 'РПГ', 'Відкритий світ'],
         video: 'https://www.youtube.com/embed/ssrNcwxALS4',
